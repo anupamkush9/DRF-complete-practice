7aacd5bdfdb1de12bda05893cea87c14341edfdb
pankajt1 <pankajt@phelix.ai>
2022-04-16 01:30:30 +0530

    all method of viewsets are implemented

--- drf_complete_practice/urls.py
+++ drf_complete_practice/urls.py
@@ -1,23 +1,27 @@
-"""drf_complete_practice URL Configuration
+"""drf_complete_practice path Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+The `pathpatterns` list routes paths to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/paths/
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    2. Add a path to pathpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a path to pathpatterns:  path('', Home.as_view(), name='home')
+Including another pathconf
+    1. Import the include() function: from django.paths import include, path
+    2. Add a path to pathpatterns:  path('blog/', include('blog.paths'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
 from first_app import views
+from rest_framework import routers
+router=routers.DefaultRouter()
+router.register('test-viewset',views.TestViewSet,basename='test-viewset')
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path(r'^api/', views.TestApiView.as_view()),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/', views.TestApiView.as_view()),
+    path('',include(router.urls))
 ]
