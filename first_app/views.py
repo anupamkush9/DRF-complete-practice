9487c4e06b6431c7ce4fbd6b493cb8de9cd5bf71
pankajt1 <pankajt@phelix.ai>
2022-04-16 21:04:41 +0530

    search-operation-implemented-via-ListApiView

--- first_app/views.py
+++ first_app/views.py
@@ -6,5 +6,18 @@ from first_app.models import Employee
 # Create your views here.
 from rest_framework import generics
 class EmployeeAPIView(generics.ListAPIView):
-    queryset=Employee.objects.all()
+    # queryset=Employee.objects.all()
     serializer_class=EmployeeSerializer
+
+    def get_queryset(self):
+        qs=Employee.objects.all()
+        name=self.request.GET.get('ename')
+        if name is not None:
+            qs=qs.filter(ename__icontains=name)
+        return qs
+
+
+
+# class EmployeeAPIView(generics.ListAPIView):
+#     queryset=Employee.objects.all()
+#     serializer_class=EmployeeSerializer
