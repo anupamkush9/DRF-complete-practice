0bb2662cd232fecf76ae61f3802ad57f6adbbeb7
pankajt1 <pankajt@phelix.ai>
2022-04-17 06:18:20 +0530

    all-mixins-are-implemented

--- first_app/views.py
+++ first_app/views.py
@@ -1,40 +1,67 @@
-from django.shortcuts import render
-from first_app.serializer import EmployeeSerializer
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from first_app.models import Employee
-# Create your views here.
+
+
+from rest_framework import mixins
 from rest_framework import generics
 
-class EmployeeCreateAPIView(generics.CreateAPIView):
-    queryset=Employee.objects.all()
-    serializer_class=EmployeeSerializer
+from first_app.models import Employee
+from first_app.serializer import EmployeeSerializer
 
-class EmployeeUpdateAPIView(generics.UpdateAPIView):
+class EmployeeListModelMixin(mixins.CreateModelMixin,mixins.ListModelMixin,
+                            generics.GenericAPIView):
     queryset=Employee.objects.all()
     serializer_class=EmployeeSerializer
-    lookup_field='id'
-class EmployeeDetailAPIView(generics.RetrieveAPIView):
+    def get(self,request,*args,**kwargs):
+        return self.list(request,*args,**kwargs)
+    def post(self,request,*args,**kwargs):
+        return self.create(request,*args,**kwargs)
+class EmployeeDetailAPIViewMixin(mixins.UpdateModelMixin,mixins.DestroyModelMixin,
+                                generics.GenericAPIView):
     queryset=Employee.objects.all()
     serializer_class=EmployeeSerializer
+    def put(self,request,*args,**kwargs):
+        return self.update(request,*args,**kwargs)
+    def patch(self,request,*args,**kwargs):
+        return self.partial_update(request,*args,**kwargs)
+    def delete(self,request,*args,**kwargs):
+        return self.destroy(request,*args,**kwargs)
 
-class EmployeeDeleteAPIView(generics.DestroyAPIView):
-    queryset=Employee.objects.all()
-    serializer_class=EmployeeSerializer
-    lookup_field='id'
-# class EmployeeAPIView(generics.ListAPIView):
-#     # queryset=Employee.objects.all()
+
+# from django.shortcuts import render
+# from first_app.serializer import EmployeeSerializer
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from first_app.models import Employee
+# # Create your views here.
+
+# class EmployeeCreateAPIView(generics.CreateAPIView):
+#     queryset=Employee.objects.all()
 #     serializer_class=EmployeeSerializer
 
-#     def get_queryset(self):
-#         qs=Employee.objects.all()
-#         name=self.request.GET.get('ename')
-#         if name is not None:
-#             qs=qs.filter(ename__icontains=name)
-#         return qs
+# class EmployeeUpdateAPIView(generics.UpdateAPIView):
+#     queryset=Employee.objects.all()
+#     serializer_class=EmployeeSerializer
+#     lookup_field='id'
+# class EmployeeDetailAPIView(generics.RetrieveAPIView):
+#     queryset=Employee.objects.all()
+#     serializer_class=EmployeeSerializer
+
+# class EmployeeDeleteAPIView(generics.DestroyAPIView):
+#     queryset=Employee.objects.all()
+#     serializer_class=EmployeeSerializer
+#     lookup_field='id'
+# # class EmployeeAPIView(generics.ListAPIView):
+# #     # queryset=Employee.objects.all()
+# #     serializer_class=EmployeeSerializer
+
+# #     def get_queryset(self):
+# #         qs=Employee.objects.all()
+# #         name=self.request.GET.get('ename')
+# #         if name is not None:
+# #             qs=qs.filter(ename__icontains=name)
+# #         return qs
 
 
 
-class EmployeeAPIView(generics.ListAPIView):
-    queryset=Employee.objects.all()
-    serializer_class=EmployeeSerializer
+# class EmployeeAPIView(generics.ListAPIView):
+#     queryset=Employee.objects.all()
+#     serializer_class=EmployeeSerializer
