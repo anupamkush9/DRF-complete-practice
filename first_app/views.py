7731a56241de033dd0cf5b661666ba6117a2ceee
Anupamkush9 <anupam.kush9@gmail.com>
2024-03-15 12:24:20 +0530

    added record into employees table

--- first_app/views.py
+++ first_app/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from first_app.models import Employee
 # Create your views here.
 from rest_framework import generics
+
 class EmployeeAPIView(generics.ListAPIView):
     queryset=Employee.objects.all()
     serializer_class=EmployeeSerializer
